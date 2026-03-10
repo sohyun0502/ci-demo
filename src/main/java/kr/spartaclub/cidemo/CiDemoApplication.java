e0cc601fe68acdb79400599f1cfca99b08c90feb
sohyun kim <sohyun050282@gmail.com>
2026-03-10 18:32:59 +0900

    first commit

---
+++ src/main/java/kr/spartaclub/cidemo/CiDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiDemoApplication.class, args);
+    }
+
+}
